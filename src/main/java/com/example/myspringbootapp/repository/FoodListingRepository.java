36eeecc396ea9372e8fa5064f32f98a42cb017f5
barbaraalp <barbaraalemes@gmail.com>
2024-02-18 17:55:32 -0300

    fix redis

--- src/main/java/com/example/myspringbootapp/repository/FoodListingRepository.java
+++ src/main/java/com/example/myspringbootapp/repository/FoodListingRepository.java
@@ -1,11 +1,7 @@
 package com.example.myspringbootapp.repository;
 
 import com.example.myspringbootapp.model.FoodListing;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 public interface FoodListingRepository extends CrudRepository<FoodListing, Long> {
 
--- src/main/java/com/example/myspringbootapp/service/FoodListingService.java
+++ src/main/java/com/example/myspringbootapp/service/FoodListingService.java
@@ -5,9 +5,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import com.example.myspringbootapp.model.FoodListing;
 import com.example.myspringbootapp.repository.FoodListingRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 import java.util.List;
 
@@ -15,20 +12,19 @@ import java.util.List;
 public class FoodListingService {
 
     private final FoodListingRepository foodListingRepository;
-    private static final Logger logger = LoggerFactory.getLogger(FoodListingService.class);
 
     @Autowired
     public FoodListingService(FoodListingRepository foodListingRepository) {
         this.foodListingRepository = foodListingRepository;
     }
-
-    @Cacheable(value = "foodListings", key = "'allFoodListings'")
+    
+    @Cacheable(value = "foodListings")
     public List<FoodListing> getAllFoodListings() {
-        logger.info("Sem cache :D");
         List<FoodListing> foodListings = (List<FoodListing>) foodListingRepository.findAll();
         return foodListings;    
     }
 
+    @Cacheable(value = "foodListing", key = "#id")
     public FoodListing getFoodListingById(Long id) {
         return foodListingRepository.findById(id).orElse(null);
     }
